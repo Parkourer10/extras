ce4840a63e6d26bbb1f4f9162d75d5d71a79a271
Park <168292059+Parkourer10@users.noreply.github.com>
2025-02-17 14:42:06 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,25 @@
+# DenOS - Debian Linux build from scratch with Debootstrap#
+# Bernardino Lopez [ bernardino.lopez@gmail.com ]
+# 11.27.18
+
+# denos_get_started.sh - Initial Pre-Req Download Script.
+# Execute the following command in a Terminal to get started the DenOS download scripts...
+# wget https://raw.githubusercontent.com/dinooz/denos/master/denos_get_started.sh
+# chmod 755 denos_get_started.sh
+# ./denos_get_started.sh
+
+wget https://github.com/Parkourer10/extras/raw/master/Denos_HowTo.txt
+wget https://github.com/Parkourer10/extras/raw/master/01_denos_pre_reqs.sh
+wget https://github.com/Parkourer10/extras/raw/master/02_denos_debootstrap.sh
+
+wget https://github.com/Parkourer10/extras/raw/master/03_denos_chroot_kde.sh
+
+# Working on this scripts...
+
+wget https://github.com/Parkourer10/extras/raw/master/04_denos_MakeSquashfs.sh
+wget https://github.com/Parkourer10/extras/raw/master/05_denos_BiosUEFI_ISO.sh
+wget https://github.com/Parkourer10/extras/raw/master/denos_config.txt
+
+
+
+chmod 755 *.sh
